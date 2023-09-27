1faa72242489d7b25ada5b55357b4d004addb559
camilozv21 <camilozv21@gmail.com>
2023-09-27 18:39:10 -0500

    add client

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -1,11 +1,38 @@
 import { useState } from "react"
 import { FaUser } from "react-icons/fa"
 import { useMutation } from "@apollo/client"
+import { ADD_CLIENT } from "../mutations/clientMutations";
+import { GET_CLIENTS } from '../queries/clientQueries'
 
 export default function AddClientModal() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
+
+    const [addClient] = useMutation(ADD_CLIENT, {
+        variables: { name, email, phone },
+        update(cache, {data: { addClient}}) {
+            const { clients } = cache.readQuery({ query: GET_CLIENTS});
+            cache.writeQuery({
+                query: GET_CLIENTS,
+                data: {clients: [...clients, addClient]}
+            })
+        }
+    })
+
+    const onsubmit = (e) => {
+        e.preventDefault();
+        
+        if(name === '' || email === '' || phone === '') {
+            return alert('Plaese fill in all fields')
+        }
+
+        addClient(name, email, phone)
+
+        setName('')
+        setEmail('')
+        setPhone('')
+    }
 
   return (
     <>
@@ -24,7 +51,7 @@ export default function AddClientModal() {
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
-                <form action="">
+                <form action="" onSubmit={onsubmit}>
                     <div className="mb-3">
                         <label className="form-label">Name</label>
                         <input type="text" className="form-control" id="name" value={name} onChange={ (e) => setName(e.target.value) }/>
@@ -38,7 +65,7 @@ export default function AddClientModal() {
                         <input type="text" className="form-control" id="phone" value={phone} onChange={ (e) => setPhone(e.target.value) }/>
                     </div>
 
-                    <button className="btn btn-secondary">Submit</button>
+                    <button type="submit" data-bs-dismiss="modal" className="btn btn-secondary">Submit</button>
                 </form>
             </div>
             </div>
