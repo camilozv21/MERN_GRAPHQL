6734b3b2856d6d704cf9a310bccbcf6e282ea797
camilozv21 <camilozv21@gmail.com>
2023-10-02 11:54:51 -0500

    update app

--- client/src/App.js
+++ client/src/App.js
@@ -42,7 +42,7 @@ function App() {
               <Route path='*' element={<NotFound />} />
             </Routes>
           </div>
-          </Router>
+        </Router>
       </ApolloProvider>
     </>
   );
